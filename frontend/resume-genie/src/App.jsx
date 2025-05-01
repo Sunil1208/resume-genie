df9311a75a0f409cb6c4162cc053e147084db093
sunil-kumar <skmy03@gmail.com>
2025-05-02 03:02:20 +0530

    added new packages and base page

--- frontend/resume-genie/src/App.jsx
+++ frontend/resume-genie/src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Auth/Login";
+
 const App = () => {
   return (
     <div>
-      
+      <Router>
+        <Routes>
+          {/* Default Route */}
+          <Route path="/" element={<LandingPage />} />
+
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
